d20d54b9d2c2124e28ae96f37d01e87c0baa4947
dell <nikolaiiislichenko@gmail.com>
2022-09-02 19:38:45 +0300

    add row to table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,23 @@ const App = () => {
             <div className="col email">Email</div>
             <div className="col data">Data</div>
           </div>
+          <div className="row">
+            <div className="col">
+              <input name="user_name" placeholder="Name"/>
+            </div>
+            <div className="col">
+              <input name="email" placeholder="Email" />
+            </div>
+            <div className="col">
+              data
+            </div>
+            <div className="cols">
+              <button className="edit-button">Edit</button>
+              <button className="delete-button">Delete</button>
+              {/*<button className="confirm-button">Confirm</button>*/}
+              {/*<button className="save-button" onClick={saveUser}>Save</button>*/}
+            </div>
+          </div>
         </div>
       </div>
     </div>
