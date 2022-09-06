61b7ef284e2bce1fe077b50c457ff0014fd3be21
dell <nikolaiiislichenko@gmail.com>
2022-09-06 12:06:47 +0300

    add function renderUsers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.scss';
 
+
 const App = () => {
-  return (
-    <div>
-      <div id="todo-list">
-        <div className="header">
-          <button className="button">Add Row</button>
-          <button className="button">Remove All</button>
-        </div>
-        <div className="table">
-          <div className="row table-header">
-            <div className="col name">Name</div>
-            <div className="col email">Email</div>
-            <div className="col data">Data</div>
-          </div>
+  const renderUsers = () => (
+    <React.Fragment>
           <div className="row">
             <div className="col">
               <input name="user_name" placeholder="Name"/>
@@ -28,13 +18,28 @@ const App = () => {
             <div className="cols">
               <button className="edit-button">Edit</button>
               <button className="delete-button">Delete</button>
-              {/*<button className="confirm-button">Confirm</button>*/}
-              {/*<button className="save-button" onClick={saveUser}>Save</button>*/}
             </div>
+          </div>
+     </React.Fragment>
+        );
+
+  return (
+    <div>
+      <div id="todo-list">
+        <div className="header">
+          <button className="button">Add Row</button>
+          <button className="button">Remove All</button>
+        </div>
+        <div className="table">
+          <div className="row table-header">
+            <div className="col name">Name</div>
+            <div className="col email">Email</div>
+            <div className="col data">Data</div>
+          </div>
+          {renderUsers()}
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
