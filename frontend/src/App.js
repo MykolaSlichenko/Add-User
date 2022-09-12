daf72ed7b6221b926a5d3e1ee374954f266d2a9a
dell <nikolaiiislichenko@gmail.com>
2022-09-12 13:13:40 +0300

    add backend and button fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,162 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
+import moment from 'moment';
+import EditLogo from './edit.svg';
+import DeleteLogo from './delete.svg';
 
+import {
+  fetchUsers,
+  postUser,
+  putUser,
+  deleteUser,
+  deleteUsers,
+} from './utils';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [editedUser, setEditedUser] = useState(null);
+  const [deleteMode, setDeleteMode] = useState(null);
+  const timestamp = new Date();
+
+  useEffect(() => {
+    async function fetchUsersApi() {
+      const data = await fetchUsers();
+      console.log(data);
+      if (data === []) {
+        setUsers('');
+      }
+      setUsers(data);
+    }
+
+    fetchUsersApi();
+  }, []);
+
+  const handleAddUser = () => {
+    const newUser = {id: 'add_new', user_name: '', email: '', data: moment(timestamp).format('L HH:mm:ss')};
+    const userWithAddNewId = users.find(user => user.id === 'add_new');
+    if (userWithAddNewId) return;
+
+    setEditedUser(newUser);
+    setUsers(users.concat(newUser));
+  };
+
+  const deleteUserConfirm = async (user_name) => {
+    await deleteUser(user_name);
+    const updatedUsers = Object.assign([], users);
+    const filteredUsers = updatedUsers.filter(user => user.user_name !== user_name);
+    setUsers(filteredUsers);
+  };
+
+  const deleteAll = async () => {
+    await deleteUsers();
+    setUsers([]);
+  };
+
+  //EDIT USER
+
+  const handleEditUser = (user) => {
+    setEditedUser(editedUser && user?.id === editedUser?.id ? null : user);
+  };
+
+  //DELETE TOGGLE
+
+  const handleDeleteToggle = (user) => {
+    setDeleteMode(deleteMode && user?.id === deleteMode?.id ? null : user);
+  };
+
+  const handleEditUserField = e => {
+    const { name, value } = e.target;
+
+    setEditedUser(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const saveUser = async () => {
+    const updatedUsers = Object.assign([], users);
+
+    if (editedUser.id === 'add_new') {
+      const data = await postUser(editedUser);
+      const index = users.findIndex(user => user.id === 'add_new');
+      updatedUsers[index] = data;
+    } else {
+      const oldUserName = users.find(user => user.id === editedUser.id).user_name;
+      const data = await putUser(oldUserName, editedUser);
+      const index = users.findIndex(user => user.id === data.id);
+      updatedUsers[index] = data;
+      setEditedUser(null);
+    }
+    setUsers(updatedUsers);
+  };
+
   const renderUsers = () => (
     <React.Fragment>
-          <div className="row">
+      {users.map((user) => {
+        const { id, user_name, email, data } = user;
+        const editMode = editedUser && editedUser.id === user.id;
+        const deleteMod = deleteMode && deleteMode.id === user.id;
+
+        const validate = () => {
+          const { user_name } = editedUser;
+          const { email } = editedUser;
+
+          //user_name
+          const isUserNameValid = user_name.trim().length > 0;
+          // const isEmailValid = email.match(/\S+@\S+\.\S+/).trim().length > 0;
+
+          const validateEmail = (email) => {
+            return String(email)
+              .toLowerCase()
+              .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              );
+          };
+
+
+          // const isValidEmail = email.match(/^\S+@\S+\.\S+$/);
+          console.log(validateEmail(email), 'validateEmail');
+
+          return !isUserNameValid || !validateEmail(email);
+        };
+
+        // const validateEmail = () => {
+        //   const { email } = editedUser;
+        //
+        //   const isEmailValid = email.trim().length > 0;
+        //
+        //   return !isEmailValid;
+        // };
+
+        return (
+          <div key={id} className="row">
             <div className="col">
-              <input name="user_name" placeholder="Name"/>
+              {editMode ? <input name="user_name" value={editedUser.user_name} onChange={handleEditUserField} /> : user_name}
             </div>
             <div className="col">
-              <input name="email" placeholder="Email" />
+              {editMode ? <input name="email" value={editedUser.email} onChange={handleEditUserField} /> : email}
             </div>
             <div className="col">
-              data
+              {data}
             </div>
             <div className="cols">
-              <button className="edit-button">Edit</button>
-              <button className="delete-button">Delete</button>
+              <button className="edit-button" onClick={() => handleEditUser(user)}><img className="logo" src={EditLogo} /></button>
+              <button className="delete-button"  onClick={() => handleDeleteToggle(user)}><img className="logo" src={DeleteLogo} /></button>
+              {deleteMod ? <button className="confirm-button" onClick={() => deleteUserConfirm(user.user_name)}>Confirm</button> : null}
+              {editMode ? <button className="save-button" onClick={saveUser} disabled={validate()} >Save</button> : null}
             </div>
           </div>
-     </React.Fragment>
-        );
+        )
+      })}
+    </React.Fragment>
+  );
 
   return (
     <div>
       <div id="todo-list">
         <div className="header">
-          <button className="button">Add Row</button>
-          <button className="button">Remove All</button>
+          <button onClick={handleAddUser} className="button">Add Row</button>
+          <button onClick={deleteAll} className="button">Remove All</button>
         </div>
         <div className="table">
           <div className="row table-header">
@@ -37,9 +165,9 @@ const App = () => {
             <div className="col data">Data</div>
           </div>
           {renderUsers()}
-          </div>
         </div>
       </div>
+    </div>
   );
 };
 
